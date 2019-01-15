6c6ca181dd78a3d9426ef38cc89d4bfd5d7deae4
LilPrice-Code <littleprice002@gmail.com>
2019-01-15 16:53:00 -0600

    Graded ClassWork

---
+++ Q3.js
@@ -0,0 +1,29 @@
+var peopleList = [
+    {
+        firstName:"Jimmy",
+        lastName:"Page",
+        age:"62",
+        zipcode:"00821",
+    },
+    {
+        firstName:"Rick",
+        lastName:"Nielsen",
+        age:"57",
+        zipcode:"61016"
+    },
+    {
+        firstName:"Jimi",
+        lastName:"Hendrix",
+        age:"58",
+        zipcode:"90001",
+        famousSong:"Purple Haze"
+    },
+    {
+        firstName:"Lemmy",
+        lastName:"Kilmister",
+        age:"57",
+        zipcode:"21120"
+    }];
+
+peopleList[0].getBandandZip = "Led Zeppelin";
+console.log(peopleList[0].getBandandZip);
